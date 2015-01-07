9c4117ba07d9aeb3122009c5fd514723c24c9d3a
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-01-07 13:45:32 +0000

    Basic implementation of interoperability for BlinkStick

---
+++ BlinkStickInterop/BlinkStick.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BlinkStickInterop
+{
+	[Guid("dfcaeff9-c582-43ca-8169-6c559516f6ab")]
+	public interface IBlinkStick
+	{
+		void SetColor (byte r, byte g, byte b);
+		void SetColorCSS (String cssColor);
+	}
+
+	[Guid("fef50ef1-729a-49bf-8342-bd6ec3a63427")]
+	[ClassInterface(ClassInterfaceType.None)]
+	public class BlinkStick : IBlinkStick
+	{
+		private BlinkStickDotNet.BlinkStick led;
+
+		public BlinkStick ()
+		{
+		}
+
+		public BlinkStick(BlinkStickDotNet.BlinkStick blinkStick)
+		{
+			led = blinkStick;
+		}
+
+		public void SetColor (byte r, byte g, byte b)
+		{
+			led.SetColor (r, g, b);
+		}
+
+		public void SetColorCSS (String cssColor)
+		{
+			led.SetColor (cssColor);
+		}
+	}
+}
+
---
+++ BlinkStickInterop/BlinkStickFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BlinkStickInterop
+{
+	[Guid("779b4d12-8d16-4218-b39c-897a0c05bf6a")]
+	public interface IBlinkStickFinder
+	{
+		IBlinkStick FindFirst ();
+		IBlinkStick FindBySerial (String serial);
+	}
+
+	[Guid("2646e9b6-4716-4f29-966c-b5c10832b64a")]
+	[ClassInterface(ClassInterfaceType.None)]
+	public class BlinkStickFinder : IBlinkStickFinder
+	{
+		public BlinkStickFinder ()
+		{
+		}
+
+		public IBlinkStick FindFirst ()
+		{
+			BlinkStickDotNet.BlinkStick blinkStick = BlinkStickDotNet.BlinkStick.FindFirst ();
+
+			if (blinkStick != null && blinkStick.OpenDevice()) {
+				return new BlinkStick (blinkStick);
+			}
+
+			return null;
+		}
+
+		public IBlinkStick FindBySerial (String serial)
+		{
+			BlinkStickDotNet.BlinkStick blinkStick = BlinkStickDotNet.BlinkStick.FindBySerial (serial);
+
+			if (blinkStick != null && blinkStick.OpenDevice()) {
+				return new BlinkStick (blinkStick);
+			}
+
+			return null;
+		}
+	}
+}
+
