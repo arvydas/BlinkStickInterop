01984ef349267b034937c43bd6d8bf7ec2a8db1e
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-01-12 20:17:07 +0000

    Added setup project and tool to generate build string

---
+++ Tools/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "/buildString")
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                object[] attributes = assembly.GetCustomAttributes(true);
+                object configRaw = attributes.FirstOrDefault(a => a.GetType() == typeof(AssemblyConfigurationAttribute));
+                if (configRaw != null) {
+                    String _ApplicationVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                    _ApplicationVersion = _ApplicationVersion.Substring(0, _ApplicationVersion.LastIndexOf('.'));
+                    _ApplicationVersion = _ApplicationVersion.Substring(0, _ApplicationVersion.LastIndexOf('.'));
+
+                    AssemblyConfigurationAttribute config = (AssemblyConfigurationAttribute)configRaw;
+                    string configuration;
+#if _WIN64
+                    configuration = "_x64";
+#else
+                    configuration = "_x86";
+#endif
+                    Console.WriteLine(_ApplicationVersion + "_" + config.Configuration + configuration);
+                    return;
+                }
+
+                Console.WriteLine("");
+                return;
+            }
+        }
+    }
+}
