7fb8e7be24e69e7361877da9b75ade39d10e270a
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-01-12 21:20:27 +0000

    Code cleanup

--- register.bat
+++ register.bat
@@ -11,11 +11,3 @@ IF %1 == "x86" (
 
 "C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file "%~2.dll" -dr INSTALLFOLDER -srd -gg -sfrag -suid -out "%~3Setup\BlinkStickInteropDLL.wxs"
 "C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file "%~2.tlb" -dr INSTALLFOLDER -srd -gg -sfrag -suid -out "%~3Setup\BlinkStickInteropTLB.wxs"
-
-
-
-REM C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /codebase /tlb $(TargetPath)
-
-REM "C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file $(TargetPath) -dr INSTALLFOLDER -srd -gg -sfrag -suid -out $(SolutionDir)Setup\BlinkStickInteropDLL.wxs
-REM "C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file $(TargetDir)$(TargetName).tlb -dr INSTALLFOLDER -srd -gg -sfrag -suid -out $(SolutionDir)Setup\BlinkStickInteropTLB.wxs
-
