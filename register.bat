184e47591a0ea98c91cd055de69467ab3fc83218
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2015-01-13 00:02:03 +0000

    Correctly generate wix setup files for DLL and TLB

--- register.bat
+++ register.bat
@@ -2,12 +2,11 @@ REM %1 - platform name
 REM %2 - TargetDir+TargetName
 REM %3 - SolutionDir
 
-
-IF %1 == "x86" (
+IF "%1" == "x86" (
 	C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /codebase /tlb "%~2.dll"
 ) ELSE (
 	C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /codebase /tlb "%~2.dll"
 )
 
-"C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file "%~2.dll" -dr INSTALLFOLDER -srd -gg -sfrag -suid -out "%~3Setup\BlinkStickInteropDLL.wxs"
-"C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file "%~2.tlb" -dr INSTALLFOLDER -srd -gg -sfrag -suid -out "%~3Setup\BlinkStickInteropTLB.wxs"
+"C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file "%~2.dll" -dr INSTALLFOLDER -srd -gg -sfrag -suid -svb6 -scom -out "%~3Setup\BlinkStickInteropDLL.wxs"
+"C:\Program Files (x86)\WiX Toolset v3.9\bin\Heat.exe" file "%~2.tlb" -dr INSTALLFOLDER -srd -gg -sfrag -suid -svb6 -scom -out "%~3Setup\BlinkStickInteropTLB.wxs"
